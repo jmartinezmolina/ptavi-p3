c7006c0992d34478e28949213d0cbe522413ee55
javier martinez molina <molina@iota17.aulas.gsyc.es>
2014-10-06 10:59:47 +0200

    comienzo definiendo clase

---
+++ smallsmilhandler.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-15 -*-
+
+# Practica3 --- Ejercicio 3   JAVIER MARTINEZ MOLINA
+
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
+
+class SmallSMILHandler(ContentHandler):
