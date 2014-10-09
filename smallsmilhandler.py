2c2346b66d912221e0c8322ec45941c173cc24d5
javier martinez molina <molina@iota19.aulas.gsyc.es>
2014-10-09 18:08:47 +0200

    inicializo variables y empiezo el startElement

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -7,3 +7,52 @@ from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
 class SmallSMILHandler(ContentHandler):
+
+#root-layout (width, height, background-color)
+#region (id, top, bottom, left, right)
+#img (src, region, begin, dur)
+#audio (src, begin, dur)
+#textstream (src, region)
+    def __init__(self):
+
+        self.width = ''
+        self.height = ''
+        self.background-color = ''
+        self.region = ''
+        self.id = ''
+        self.top = ''
+        self.bottom = ''
+        self.left = ''
+        self.right = ''
+        self.img = ''
+        self.src = ''
+        self.begin = ''
+        self.dur = ''
+        self.audio = ''
+        self.textstream = ''
+        
+    def startElement(self, name, attrs):
+    
+        if name == 'root-layout':
+            self.width = attrs.get('width', "")
+            self.height = attrs.get('height', "")
+            self.background-color = attrs.get('background-color', "")
+            
+        elif name == 'region':
+            self.id = attrs.get('id', "")
+            self.top = attrs.get('top', "")
+            self.bottom = attrs.get('bottom', "")
+            self.left = attrs.get('left', "")
+            self.right = attrs.get('right', "")
+            
+        elif name == 'img':
+            self.src = attrs.get('src', "")
+            self.region = attrs.get('region', "")
+            self.begin = attrs.get('begin', "")
+            self.dur = attrs.get('dur', "")
+            
+        elif name == 'audio':
+        elif name == 'textstream':
+            
+            
+            
