54dbf482a82f4a5d8e7a3d2329c52a737bbc778a
javier martinez molina <molina@iota19.aulas.gsyc.es>
2014-10-10 18:39:41 +0200

    ejercicio 3 terminado y pep8 pasado

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -6,83 +6,37 @@
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
+
 class SmallSMILHandler(ContentHandler):
 
     def __init__(self):
 
-        self.width = ""
-        self.height = ""
-        self.background_color = ""
-        self.region = ""
-        self.id = ""
-        self.top = ""
-        self.bottom = ""
-        self.left = ""
-        self.right = ""
-        self.img = ""
-        self.src = ""
-        self.begin = ""
-        self.dur = ""
-        self.end = ""
-        self.audio = ""
-        self.textstream = ""
-        self.fill = ""
-        
-    def startElement(self, name, attrs):
-    
-        if name == 'root-layout':
-            self.width = attrs.get('width', "")
-            self.height = attrs.get('height', "")
-            self.background_color = attrs.get('background-color', "")
-            print self.width
-            print self.height
-            print self.background_color
-            
-        elif name == 'region':
-            self.id = attrs.get('id', "")
-            self.top = attrs.get('top', "")
-            self.bottom = attrs.get('bottom', "")
-            self.left = attrs.get('left', "")
-            self.right = attrs.get('right', "")
-            print self.id
-            print self.top
-            print self.bottom
-            print self.left
-            print self.right
-            
-        elif name == 'img':
-            self.src = attrs.get('src', "")
-            self.region = attrs.get('region', "")
-            self.begin = attrs.get('begin', "")
-            self.dur = attrs.get('dur', "")
-            self.end = attrs.get('end', "")
-            print self.src
-            print self.region
-            print self.begin
-            print self.dur
-            print self.end
+        self.etiquetas = {
+            'root-layout': ['width', 'height', 'background-color'],
+            'region': ['id', 'top', 'bottom', 'left', 'right'],
+            'img': ['src', 'region', 'begin', 'dur', 'end'],
+            'audio': ['src', 'begin', 'dur'],
+            'textstream': ['src', 'region', 'fill']}
 
-        elif name == 'audio':
-            self.src = attrs.get('src', "")
-            self.begin = attrs.get('begin', "")
-            self.dur = attrs.get('dur', "")
-            print self.src
-            print self.begin
-            print self.dur
-            
-        elif name == 'textstream':
-            self.src = attrs.get('src', "")
-            self.region = attrs.get('region', "")
-            self.fill = attrs.get('fill', "")
-            print self.src
-            print self.region
-            print self.fill
+        self.list_etiquetas = []
+
+    def startElement(self, name, attrs):
+
+        dic = {}
+        if name in self.etiquetas:
+            dic["name"] = name
+            for atributo in self.etiquetas[name]:
+                dic[atributo] = attrs.get(atributo, "")
+            self.list_etiquetas.append(dic)
+
+    def get_tags(self):
+
+        return self.list_etiquetas
 
 if __name__ == "__main__":
-    """
-    Programa principal
-    """
+
     parser = make_parser()
     chandler = SmallSMILHandler()
     parser.setContentHandler(chandler)
     parser.parse(open('karaoke.smil'))
+    print chandler.get_tags()
