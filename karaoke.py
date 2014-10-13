16b67bb12fb4bfa36c061f411fd2c6dad68c4863
javier martinez molina <molina@iota39.aulas.gsyc.es>
2014-10-13 16:09:46 +0200

    apartado 6: acabado el ejercicio con return en el metodo __str__

--- karaoke.py
+++ karaoke.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Ejercicio4 --- Práctica 3 JAVIER MARTÍNEZ MOLINA
+# Ejercicios 4, 5, 6 --- Práctica 3 JAVIER MARTÍNEZ MOLINA
 
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
@@ -10,33 +10,45 @@ import sys
 import smallsmilhandler
 
 
-if len(sys.argv) != 2:
-    print('Usage: python karaoke.py file.smil.\n')
-    raise SystemExit
+class KaraokeLocal(smallsmilhandler.SmallSMILHandler):
 
-parser = make_parser()
-chandler = smallsmilhandler.SmallSMILHandler()
-parser.setContentHandler(chandler)
-parser.parse(open(sys.argv[1]))
-lista_dic = chandler.get_tags()
+    def __init__(self, fichero):
+
+        parser = make_parser()
+        chandler = smallsmilhandler.SmallSMILHandler()
+        parser.setContentHandler(chandler)
+        parser.parse(open(fichero))
+        self.lista_dic = chandler.get_tags()
+
+    def __str__(self):
+        salida = ""
+        for dic in self.lista_dic:
+            salida += dic["name"] + "\t"
+            for etiqueta in dic:
+                if dic["name"] != dic[etiqueta] and dic[etiqueta] != "":
+                    salida += etiqueta + "=" + '"' + dic[etiqueta] + '"' + "\t"
+            salida += "\n"
+        salida += "\n"
+        return salida
+
+    def do_local(self):
+
+        for dic in self.lista_dic:
+            for etiqueta in dic:
+                if etiqueta == "src":
+                    recurso = dic[etiqueta]
+                    os.system("wget -q " + recurso)
+                    elem_div = recurso.split('/')
+                    dic[etiqueta] = elem_div[-1]
 
 
-for dic in lista_dic:
-    for etiqueta in dic:
-        if etiqueta == "src":
-            recurso = dic[etiqueta]
-            os.system("wget -q " + recurso)
-            elem_div = recurso.split('/')
-            dic[etiqueta] = elem_div[-1]
+if __name__ == '__main__':
 
-for dic in lista_dic:
-    print dic["name"] + "\t",
-    for etiqueta in dic:
-        if dic["name"] != dic[etiqueta] and dic[etiqueta] != "":
-            print etiqueta + "=" + '"' + dic[etiqueta] + '"' + "\t",
-    print 
-            
+    if len(sys.argv) != 2:
+        print('Usage: python karaoke.py file.smil.\n')
+        raise SystemExit
 
-
-
-
+    karaoke = KaraokeLocal(sys.argv[1])
+    print karaoke
+    karaoke.do_local()
+    print karaoke
